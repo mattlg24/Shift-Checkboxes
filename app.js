c39e37ded72a1837bea393b7e1b849776aace519
Matt Gardner <matthewlgardner@gmail.com>
2017-04-21 12:32:28 -0600

    log the event and if shift key was pressed

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 window.onload = function() {
 
+    let checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]')
 
+    function handleCheck(e) {
+        console.log(e);
+        console.log(e.shiftKey);
+    }
 
+    checkboxes.forEach(checkbox => {
+        checkbox.addEventListener('click', handleCheck)
+    })
 }
