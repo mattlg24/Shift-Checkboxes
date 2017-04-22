61c64cdf0ce9b3206a3f393aab15fbbb851b5ab0
Matt Gardner <matthewlgardner@gmail.com>
2017-04-22 14:32:19 -0600

    check multiple boxes with shiftkey

--- app.js
+++ app.js
@@ -2,9 +2,26 @@ window.onload = function() {
 
     let checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]')
 
+    let lastChecked
+
     function handleCheck(e) {
-        console.log(e);
-        console.log(e.shiftKey);
+
+        let inBetween = false
+
+        if (e.shiftKey && this.checked) {
+
+            checkboxes.forEach(checkbox => {
+
+                if (checkbox === this || checkbox === lastChecked) {
+                  inBetween = !inBetween
+                }
+
+                if (inBetween) {
+                  checkbox.checked = true
+                }
+            })
+        }
+        lastChecked = this
     }
 
     checkboxes.forEach(checkbox => {
